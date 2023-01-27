07b62319cd2137a4bfd4502db563946d38f931d4
hiranwj <hiranwj.connect@gmail.com>
2023-01-28 01:44:46 +0530

    Add binary search and implement the logic

--- binary-search/src/main/java/lk/hiranwj/BinarySearchDemo1.java
+++ binary-search/src/main/java/lk/hiranwj/BinarySearchDemo1.java
@@ -1,24 +1,39 @@
 package lk.hiranwj;
 
-import java.util.Arrays;
-
 public class BinarySearchDemo1 {
 
     public static void main(String[] args) {
-        int[] arr = new int[1000];
+        int[] arr = new int[100000];
 
-        int target = 10;
+        int target = 49000;
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
         }
 
-        int index = Arrays.binarySearch(arr, target);
+//        int index = Arrays.binarySearch(arr, target);
+        int index = binarySearch(arr, target);
 
         if (index == -1) {
             System.out.println("Value not found...!");
         }else {
             System.out.println("Value found at the index of: " + index);
         }
+    }
+
+    private static int binarySearch(int[] arr, int target) {
+            int min = 0;
+            int max = arr.length - 1;
+
+            while (min <= max) {
+                int middle = min + (max - min) / 2;
+                int value = arr[middle];
+                System.out.println("Middle value is: " + value);
+
+                if (value < target) min = middle + 1;
+                else if (value > target) max = middle - 1;
+                else return middle;
+            }
+        return -1;
     }
 }
